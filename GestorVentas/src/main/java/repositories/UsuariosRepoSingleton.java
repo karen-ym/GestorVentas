618f4dd811d4631be5a0e1f915bb744e6ef49aed
JulietaMereles <166165422+JulietaMereles@users.noreply.github.com>
2024-11-25 13:22:56 -0300

    Se agrego las funciones en UsuariosRepoSingleton

--- GestorVentas/src/main/java/repositories/UsuariosRepoSingleton.java
+++ GestorVentas/src/main/java/repositories/UsuariosRepoSingleton.java
@@ -1,7 +1,59 @@
 package repositories;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Usuario;
 import repositories.interfaces.UsuariosRepo;
 
 public class UsuariosRepoSingleton implements UsuariosRepo{
+	
+	private static UsuariosRepoSingleton singleton;
+	
+	public static UsuariosRepoSingleton getInstance() {
+		if(singleton == null) {
+			singleton = new UsuariosRepoSingleton();
+		}
+		return singleton;
+	}
+	
+	private List<Usuario> listaUsuarios;
+	
+	private UsuariosRepoSingleton() {
+		this.listaUsuarios = new ArrayList<Usuario>();
+	}
+
+	@Override
+	public List<Usuario> getAll() {
+		
+		return new ArrayList<Usuario>(this.listaUsuarios);
+	}
+
+	@Override
+	public Usuario findById(int id) {
+		
+		return this.listaUsuarios.stream()
+		.filter((e)-> e.getId() == id )
+		.findAny()
+		.orElse(null);
+	}
+
+	@Override
+	public void insert(Usuario usuario) {
+		
+		int ultimaId = this.listaUsuarios.stream()
+				.map(Usuario::getId)
+				.max(Integer::compare)
+				.orElse(0);
+		
+		usuario.setId(ultimaId+1);
+		
+		this.listaUsuarios.add(usuario);
+	}
+
+	@Override
+	public void delete(int id) {
+		this.listaUsuarios.removeIf((u)-> u.getId() == id);
+	}
 
 }
