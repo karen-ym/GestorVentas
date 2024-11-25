c62ae7ec835d8d677605a4645e36e8954f9531e5
JulietaMereles <166165422+JulietaMereles@users.noreply.github.com>
2024-11-25 13:20:09 -0300

    Se agrego las funciones en UsuarioController

--- GestorVentas/src/main/java/controllers/UsuariosController.java
+++ GestorVentas/src/main/java/controllers/UsuariosController.java
@@ -1,41 +1,114 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UsuariosController
- */
-@WebServlet("/UsuariosController")
+import models.Usuario;
+import repositories.UsuariosRepoSingleton;
+import repositories.interfaces.UsuariosRepo;
+
+@WebServlet("/usuarios")
 public class UsuariosController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private UsuariosRepo usuariosRepo;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public UsuariosController() {
-        super();
-        // TODO Auto-generated constructor stub
+    	this.usuariosRepo = UsuariosRepoSingleton.getInstance();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		String accion = request.getParameter("accion");
+		accion = Optional.ofNullable(accion).orElse("index");
+		
+		switch (accion) {
+		case "index"-> getIndex(request, response);
+		case "show"-> getShow(request, response);
+		case "create"-> getCreate(request, response);
+		default -> response.sendError(404);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	private void getCreate(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		request.getRequestDispatcher("views/Usuarios/Create.jsp").forward(request, response);
+		
+
 	}
 
+
+	private void getShow(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		String sId = request.getParameter("id");
+		int id = Integer.parseInt(sId);
+		
+		Usuario usu = usuariosRepo.findById(id);
+		
+		request.setAttribute("usuario", usu);
+		
+		request.getRequestDispatcher("views/Usuarios/show.jsp").forward(request, response);
+
+	}
+
+	private void getIndex(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+		
+		UsuariosRepo repo = UsuariosRepoSingleton.getInstance();
+		
+		List<Usuario> listaUsuarios = repo.getAll();
+		
+		request.setAttribute("listarda", listaUsuarios);
+		
+		request.getRequestDispatcher("/views/Usuarios/index.jsp").forward(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		String accion = request.getParameter("accion");
+		accion = Optional.ofNullable(accion).orElse("insert");
+		
+		switch (accion) {
+		case "insert" -> postInsert(request, response);
+		case "delete" -> postDelete(request, response);
+		default -> response.sendError(404, "No existe accion: "+accion);
+		}
+		
+	}
+
+	private void postDelete(HttpServletRequest request, HttpServletResponse response) {
+		String sId = request.getParameter("id");
+		int id = Integer.parseInt(sId);
+		
+		usuariosRepo.delete(id);
+	}
+
+	private void postInsert(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		String nombreUsuario = request.getParameter("nombreUsuario");
+		
+		String contrasenia = request.getParameter("contrasenia");
+		
+		String sSaldoActual = request.getParameter("saldoActual");
+		double saldo =Double.parseDouble(sSaldoActual);
+		
+		String tipo = request.getParameter("tipo");
+		
+		Usuario usu = new Usuario(nombreUsuario, contrasenia, tipo, saldo);
+	
+		usuariosRepo.insert(usu);
+		
+		response.sendRedirect("usuarios");
+
+}
 }
