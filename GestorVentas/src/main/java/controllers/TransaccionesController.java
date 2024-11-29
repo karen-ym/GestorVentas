4740a56aaf4bab19fde98a27f917f7cb192c3abc
JorgePomaV <jpomavergara1418@gmail.com>
2024-11-29 14:52:40 -0300

    Se agrego metodos POST a transaccionesController

--- GestorVentas/src/main/java/controllers/TransaccionesController.java
+++ GestorVentas/src/main/java/controllers/TransaccionesController.java
@@ -1,6 +1,10 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,7 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Transaccion;
+import models.Usuario;
+import repositories.TransaccionesRepoSingleton;
+import repositories.UsuariosRepoSingleton;
 import repositories.interfaces.TransaccionesRepo;
+import repositories.interfaces.UsuariosRepo;
 
 /**
  * Servlet implementation class TransaccionesController
@@ -17,24 +25,84 @@ import repositories.interfaces.TransaccionesRepo;
 public class TransaccionesController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private TransaccionesRepo transaccionesRepo;
+	private UsuariosRepo usuariosRepo;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public TransaccionesController() {
-        super();
-        // TODO Auto-generated constructor stub
+	      this.transaccionesRepo = TransaccionesRepoSingleton.getInstance();
+	      this.usuariosRepo = UsuariosRepoSingleton.getInstance();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		String accion = request.getParameter("accion");
+		accion = Optional.ofNullable(accion).orElse("index");
+		
+		switch (accion) {
+		case "index" -> getIndex(request,response);
+		case "transferir" -> getTransferir(request,response);
+		case "registros" -> getRegistros(request,response);
+		case "show" -> getShow(request,response);
+		default ->response.sendError(404);
+		}
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	
+	private void getShow(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+
+	//Manda los datos de Registro de las transacciones (realizadas, recibidas) de un usuario particular.
+	private void getRegistros(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int idDeudor = Integer.parseInt(request.getParameter("idUsuario"));
+		
+		transaccionesRepo.obtenerTransaccionesPorUsuarioDeudor(idDeudor);
+		List<Transaccion> beneficiario = transaccionesRepo.obtenerTransaccionesPorUsuarioBeneficiario(idDeudor);
+		List<Transaccion> deudor = transaccionesRepo.obtenerTransaccionesPorUsuarioBeneficiario(idDeudor);
+		
+		beneficiario = beneficiario != null ? beneficiario : new ArrayList<Transaccion>();
+		deudor = deudor != null ? deudor : new ArrayList<Transaccion>();
+		
+		request.setAttribute("beneficiario", beneficiario);
+		request.setAttribute("deudor", deudor);
+		
+		request.getRequestDispatcher("").forward(request, response);//falta implementar la vista
+	}
+
+	//Manda los datos del usuario a la vista donde se transfiere.
+	private void getTransferir(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		int idDeudor = Integer.parseInt(request.getParameter("idDeudor"));
+		
+		Usuario usuario = usuariosRepo.findById(idDeudor);
+		
+		if(usuario == null) {
+			response.sendError(404, "Usuario no encontrado"); 
+			return;
+		}
+		request.setAttribute("usuario", usuario);
+		request.getRequestDispatcher("").forward(request, response);//falta implementar la vista
+	}
+
+	//Manda los datos del usuario a la vista.
+	private void getIndex(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int idUsuario = Integer.parseInt(request.getParameter("idUsuario"));
+		
+		Usuario usuario = usuariosRepo.findById(idUsuario);
+		if(usuario == null) {
+			response.sendError(404, "Usuario no encontrado"); 
+			return;
+		}
+		request.setAttribute("usuario", usuario);
+		
+		request.getRequestDispatcher("").forward(request, response);//falta implementar la vista
+	}
+
 	@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
