a6ccf6daa1e52ddadc94f3da9b09fac25f7d1e7d
JulietaMereles <166165422+JulietaMereles@users.noreply.github.com>
2024-11-30 14:06:16 -0300

    Se agrego un findById en TransaccionesRepo

--- GestorVentas/src/main/java/repositories/interfaces/TransaccionesRepo.java
+++ GestorVentas/src/main/java/repositories/interfaces/TransaccionesRepo.java
@@ -10,6 +10,6 @@ public interface TransaccionesRepo {
     public void registrarTransaccion(Transaccion transaccion); // 1
     List<Transaccion> obtenerTransaccionesPorUsuarioDeudor(int idUsuario); // 2 
     List<Transaccion> obtenerTransaccionesPorUsuarioBeneficiario(int idUsuario); // 3
-    
+    Transaccion findById(int id); // 4
     // Los numeros son para que no me olvide -K
 }
